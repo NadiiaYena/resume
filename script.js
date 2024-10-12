bddb6bdc3881f5d0b25b366656143fd97c63213e
Nadiia-Y <nadezhda.yena@gmail.com>
2024-10-12 19:03:45 +0300

    updated

--- script.js
+++ script.js
@@ -9,10 +9,31 @@ fetch('https://ipapi.co/json/')
             if (data.country_name === 'Ukraine') {
                 console.log(data.country_name)
                
-                photo.style.display = 'block';
+                photo.style.display = 'flex';
             }
         })
         .catch(error => {
             return null
             // console.error('Помилка при отриманні геолокації:', error);
         });
+
+
+// Отримати елемент кнопки
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+// Відстежувати прокрутку сторінки
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = 'block'; // Показати кнопку, коли прокрутка більше 100px
+  } else {
+    scrollToTopBtn.style.display = 'none'; // Сховати кнопку, коли прокрутка менше 100px
+  }
+};
+
+// Додати обробник події для прокрутки до верху
+scrollToTopBtn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Плавна прокрутка
+  });
+};
